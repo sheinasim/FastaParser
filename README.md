6fde98e4bce944bdd9528bdefeafbdbd1163469e
sheinasim <ssim8@hawaii.edu>
2021-10-27 20:28:19 -1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# FindDuplicatesFromBUSCO 
+Extracts records from a fasta based on a list of fasta headers.
+
+Dependencies:
+
+* Python3 
+* Biopython
+* Python pandas
+
+### Usage
+  
+> python fastaParser.py \<assembly.fasta\> \<records.txt\>
+
+If no arguments are provided, the script will return help message.
+
+## Outputs
+
+* \{records file prefix\}\_records.fasta
+* \{records file prefix\}\_records.lengths
+* \{records file prefix\}\_remaining.fasta
+* \{records file prefix\}\_remaining.lengths
+
+### Citation
+
+If this script is useful to you, please cite the following in your publication:
+
+```
+@software{FastaParser,
+  author = {Sim, Sheina B.},
+  title = {FastaParser},
+  url = {https://github.com/sheinasim/FastaParser}
+}
+```
+
+Sheina B. Sim  
+USDA-ARS  
+US Pacific Basin Agricultural Research Service  
+Hilo, Hawaii, 96720 USA  
+sheina.sim@usda.gov  
+
+This script is in the public domain in the United States per 17 U.S.C. § 105
---
+++ fastaparser.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import pandas as pd
+import argparse
+from Bio import SeqIO
+
+parser = argparse.ArgumentParser()
+parser.add_argument("fasta", help=".fasta file to parse.")
+parser.add_argument("records", help="File containing fasta headers to isolate.")
+args = parser.parse_args()
+
+input_file = open(args.records, "r")
+content = input_file.read()
+content_list = content.split("\n")
+input_file.close()
+
+sequences_in_file = []
+sequences_not_in_file = []
+
+df_record_lengths = pd.DataFrame(columns=('record', 'length'))
+for record in SeqIO.parse(args.fasta, "fasta"):
+        df_record_lengths = df_record_lengths.append({'record' : record.name, 'length' : len(record.seq)}, ignore_index = True)
+        if record.id.split(" ")[0] in content_list:
+            sequences_in_file.append(record)
+        else:
+            sequences_not_in_file.append(record)
+            
+df1 = df_record_lengths[df_record_lengths['record'].isin(content_list)] 
+df2 = df_record_lengths[~df_record_lengths['record'].isin(content_list)] 
+
+outfile1 = args.records.split(".")[0] + "_records"
+outfile2 = args.records.split(".")[0] + "_remaining"
+
+SeqIO.write(sequences_in_file, outfile1 + ".fasta", "fasta")
+SeqIO.write(sequences_not_in_file, outfile2 + ".fasta", "fasta")
+df1.to_csv(outfile1 + ".lengths", index=False, sep='\t', header=True)
+df2.to_csv(outfile2 + ".lengths", index=False, sep='\t', header=True)
+
+print("fastaparser finished parsing.")
